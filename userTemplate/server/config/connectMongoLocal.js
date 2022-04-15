e392f21c7d9a0b15bba7ead02b6d46cf59f73003
Vicente <viperalta@gmail.com>
2022-04-15 01:22:24 -0400

    remove user project corrupted

--- userTemplate/server/config/connectMongoLocal.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DB_LINK, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log("BASES DE DATOS OPERACIONAL"))
-	.catch(err => console.log("Algo salió mal", err));
