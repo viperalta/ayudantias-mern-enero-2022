11967e3339bc2ba0952a4f4580837b79e4df0958
Vicente <viperalta@gmail.com>
2022-10-23 13:03:48 -0300

    actualizacion de repo

--- 6 Abril/peliculas-manager/client/src/actions/simpleGet.js
+++
@@ -1,9 +0,0 @@
-export async function simpleGet (url) {
-    try{
-        console.log("llegue")
-        const apiResponse= await fetch(url).then((response)=>response.json());
-        return apiResponse
-    }catch(error){
-        return {error:"se ha producido un error:"+error}
-    }
-}
